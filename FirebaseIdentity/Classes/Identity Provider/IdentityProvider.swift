95a8f83c4b23c10ffffd2b888090724c889ff18e
Christian Gossain <cgossain@gmail.com>
2021-04-06 13:43:46 -0600

    Define nonce generation functions as an extension to the IdentityProvider protocol

--- FirebaseIdentity/Classes/Identity Provider/IdentityProvider.swift
+++ FirebaseIdentity/Classes/Identity Provider/IdentityProvider.swift
@@ -24,6 +24,7 @@
 
 import Foundation
 import FirebaseAuth
+import CryptoKit
 
 public protocol IdentityProvider {
     /// The provider ID of the receiver.
@@ -40,4 +41,50 @@ public protocol IdentityProvider {
     
     /// Links the currently signed in user with the credentials specified by the receiver.
     func link(completion: @escaping AuthDataResultCallback)
+}
+
+
+@available(iOS 13.0, *)
+extension IdentityProvider {
+    /// Generates and returns a hashed version of the input string using the SHA256 algorithm.
+    static public func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap { return String(format: "%02x", $0) }.joined()
+      return hashString
+    }
+    
+    /// Generates and returns a cryptographically randon nonce.
+    ///
+    /// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    static public func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
 }
--- FirebaseIdentity/Classes/Identity Provider/Providers/SignInWithAppleIdentityProvider.swift
+++ FirebaseIdentity/Classes/Identity Provider/Providers/SignInWithAppleIdentityProvider.swift
@@ -24,7 +24,6 @@
 
 import Foundation
 import AuthenticationServices
-import CryptoKit
 import FirebaseAuth
 
 public enum SignInWithAppleIdentityProviderError: Error {
@@ -136,53 +135,5 @@ public final class SignInWithAppleIdentityProvider: IdentityProvider {
                                                   rawNonce: nonce)
         
         currentUser.link(with: credential, completion: completion)
-    }
-}
-
-@available(iOS 13.0, *)
-extension SignInWithAppleIdentityProvider {
-    static public func sha256(_ input: String) -> String {
-      let inputData = Data(input.utf8)
-      let hashedData = SHA256.hash(data: inputData)
-      let hashString = hashedData.compactMap {
-        return String(format: "%02x", $0)
-      }.joined()
-
-      return hashString
-    }
-    
-    /// Generates and returns a cryptographically randon nonce.
-    ///
-    /// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-    static public func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        let charset: Array<Character> =
-            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        var result = ""
-        var remainingLength = length
-        
-        while remainingLength > 0 {
-            let randoms: [UInt8] = (0 ..< 16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-                }
-                return random
-            }
-            
-            randoms.forEach { random in
-                if remainingLength == 0 {
-                    return
-                }
-                
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
-            }
-        }
-        
-        return result
     }
 }
