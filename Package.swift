66ed32974fe41a5f6c6bdde1af346969873dbdf3
Christian Gossain <cgossain@gmail.com>
2024-08-29 23:59:04 -0600

    Update Firebase SDK version in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -13,21 +11,15 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/firebase/firebase-ios-sdk.git",
-            from: "9.0.0"
-        ),
-        .package(
-            url: "https://github.com/ProcedureKit/ProcedureKit.git",
-            from: "5.0.0"
-        ),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.1.0"),
+        .package(url: "https://github.com/ProcedureKit/ProcedureKit.git", from: "5.2.0")
     ],
     targets: [
         .target(
             name: "FirebaseIdentity",
             dependencies: [
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-                .product(name: "ProcedureKit", package: "ProcedureKit"),
+                .product(name: "ProcedureKit", package: "ProcedureKit")
             ]
         ),
         .testTarget(
